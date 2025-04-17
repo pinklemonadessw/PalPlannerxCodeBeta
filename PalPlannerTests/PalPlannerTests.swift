d39192089c07f8aeb25d9621f972cd962d05a47e
Pink Lemonade <158772163+pinklemonadessw@users.noreply.github.com>
2025-04-16 23:17:18 -0500

    Initial Commit

---
+++ PalPlannerTests/PalPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PalPlannerTests.swift
+//  PalPlannerTests
+//
+//  Created by Logan Leatherwood on 4/16/25.
+//
+
+import Testing
+@testable import PalPlanner
+
+struct PalPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
